478e845e65ce648bf64111407a356a1220269da6
mdm106 <mads.mcmahon@googlemail.com>
2020-06-26 11:42:11 +0100

    Resource for game created and populated

---
+++ app/Http/Resources/API/GameResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\API;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class GameResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            "id" => $this->id,
+            "game_date" => $this->game_date,
+            "team_1" => $this->team_1,
+            "team_2" => $this->team_2,
+            "team_1_score" => $this->team_1_score,
+            "team_2_score" => $this->team_2_score,
+        ];
+    }
+}
